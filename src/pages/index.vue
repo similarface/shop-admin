dff658d29f068b5e54b2675c92ef87d51da9479f
similarface <yangwubing@molbreeding.com>
2023-01-28 16:16:10 +0800

    3.5 setup语法糖组合式API

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
-        good
+        good {{count}}
+        <el-button @click="addCount">+1</el-button>
+        addCount2 use reactive {{form.count}}
+        <el-button @click="addCount2">+1</el-button>
     </div>
 </template>
+<script setup>
+    import { ref, reactive } from "vue"
+    // 普通类型变量
+    let count = ref(1)
+    // 复杂类型变量
+    let form = reactive({
+        count:1
+    })
+    function addCount(){
+        count.value++
+        console.log("addCount")
+    }
+    function addCount2(){
+        form.count++
+        console.log("addCount2")
+    }
+</script>
